f6a4d1e51e02b16c2c957a07ab154f15b0b89b1d
grahaminn <grahaminn@hotmail.com>
2014-06-07 18:00:42 +0100

    add means to exercise 2 from 2.2

--- ex2-2-2.py
+++ ex2-2-2.py
@@ -21,8 +21,9 @@ for record in table.records:
 first_births_mean, first_births_variance = thinkstats.MeanVar(first_births)
 other_births_mean, other_births_variance = thinkstats.MeanVar(other_births)
 
+print 'mean of first birth gestation=', first_births_mean
 print 'std deviation of first birth gestation=', math.sqrt(first_births_variance)
-
+print 'mean of other birth gestation=', other_births_mean
 print 'std deviation of other birth gestation=', math.sqrt(other_births_variance)
 
     
