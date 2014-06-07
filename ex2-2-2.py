7f42c0f6fa7a84840c6f9dff13684395ea2ae9e4
grahaminn <grahaminn@hotmail.com>
2014-06-07 17:58:20 +0100

    exercise 2 from 2.2

---
+++ ex2-2-2.py
@@ -0,0 +1,28 @@
+import csv
+import math
+import survey
+import thinkstats
+
+table = survey.Pregnancies()
+table.ReadRecords()
+
+first_births = []
+other_births = []
+
+for record in table.records:
+    if record.outcome != 1:
+        continue
+   
+    if record.birthord == 1:
+        first_births.append(record.prglength)
+    else:
+        other_births.append(record.prglength)
+
+first_births_mean, first_births_variance = thinkstats.MeanVar(first_births)
+other_births_mean, other_births_variance = thinkstats.MeanVar(other_births)
+
+print 'std deviation of first birth gestation=', math.sqrt(first_births_variance)
+
+print 'std deviation of other birth gestation=', math.sqrt(other_births_variance)
+
+    
