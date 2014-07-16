72886bd33dd01cd5fb7a4a59f90292c23cc55f75
grahaminn <grahami@blinkbox.com>
2014-07-16 16:55:13 +0100

    update risk and first

--- risk.py
+++ risk.py
@@ -1,19 +1,28 @@
-include Pmf
+import descriptive
+import Pmf
+
+def FilteredCumulativeProbability(filter_func, pmf):
+    filtered_items = filter(filter_func, pmf.Items())
+    filtered_probabilities = map(lambda x:x[1], filtered_items)
+    return sum(filtered_probabilities) 
 
 def ProbEarly(pmf):
-    early_births = filter(lambda x: x[0] <= 37 , pmf.Items())
-    earlyPmf = Pmf.MakePmfFromDict(dict(early_births))
-    earlyPmf.Normalize()
-    return earlyPmf
+    return FilteredCumulativeProbability(lambda x: x[0] <= 37, pmf)
 
 def ProbOnTime(pmf):
-    on_time_births = filter(lambda x: x[0] >= 38 and x[0] <= 40 , pmf.Items())
-    onTimePmf = Pmf.MakePmfFromDict(dict(on_time_births))
-    onTimePmf.Normalize()
-    return onTimePmf
+    return FilteredCumulativeProbability(lambda x: x[0] >= 38 and x[0] <= 40 , pmf)
 
 def ProbLate(pmf):
-    late_births = filter(lambda x: x[0] > 40 , pmf.Items())
-    latePmf = Pmf.MakePmfFromDict(dict(late_births))
-    latePmf.Normalize()
-    return latePmf
+    return FilteredCumulativeProbability(lambda x: x[0] > 40 , pmf)
+
+def RelativeRisk(pmf_first, pmf_other):
+    print 'Prob of being early for first births %f, for others %f' % (ProbEarly(pmf_first),ProbEarly(pmf_other))
+    print 'Prob of being on time for first births %f, for other %f' % (ProbOnTime(pmf_first),ProbOnTime(pmf_other))
+    print 'Prob of being late for first births %f, for others %f' % (ProbLate(pmf_first), ProbLate(pmf_other))
+
+def main():
+    pool, firsts, others = descriptive.MakeTables()
+    RelativeRisk(firsts.pmf, others.pmf)
+
+if __name__ == "__main__":
+    main()
