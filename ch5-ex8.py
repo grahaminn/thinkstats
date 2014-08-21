56348731e68b59f732f510ac5e1ae31959e0622c
grahaminn <grahami@blinkbox.com>
2014-08-21 20:52:08 +0100

    questions 8 & 9

---
+++ ch5-ex8.py
@@ -0,0 +1,5 @@
+# Probability of rolling at least one 6 on two dice is the compliment of rolling no 6s
+
+print 'probability of at least one 6=', (2 * (1.0 / 6.0)) - ((1.0 / 6.0) ** 2)
+
+
---
+++ ch5-ex9.py
@@ -0,0 +1,3 @@
+# The probability of exactly one 6 from two dice rolls is equal to the product of a 6 and 'not a 6', twice
+
+print 'probability of exactly one 6', (1.0 / 6.0) * (5.0 / 6.0) * 2
