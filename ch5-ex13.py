49e5dee270d1ead36d7c47b9502336765189380e
grahaminn <grahami@blinkbox.com>
2014-08-22 21:45:49 +0100

    update to monte carlo

--- ch5-ex13.py
+++ ch5-ex13.py
@@ -5,26 +5,39 @@ import numpy
 import operator
 import Pmf
 
-def year_of_cohort(people_in_cohort):
+def run_cohort(people, years):
     number_infected = 0
-    for person in range(people_in_cohort):
-        if numpy.random.randint(1000) == 1:
-            number_infected += 1
-    return number_infected 
+    for year in range(years):
+        for person in range(people):
+            if numpy.random.randint(1000) == 1:
+                number_infected += 1
+    return number_infected
+
+def monte_carlo_cohorts(people, cohorts, years, events_required, experiments):
+    experimental_successes = 0.0
+    for experiment in range(experiments):
+        experiment_succeeded = False
+        for cohort in range(cohorts):
+            if run_cohort(people, years) >= events_required:
+                experimental_successes += 1.0
+                break
+
+    return experimental_successes / experiments     
 
 cohort_dict = defaultdict(int)
 
-for year in range(10):
-    for cohort in range(10000):
-        cohort_dict[cohort] += year_of_cohort(100) 
+for cohort in range(100000):
+    cohort_dict[cohort] += run_cohort(people=100, years=10) 
 
 pmf = Pmf.MakePmfFromList(cohort_dict.values())
 cdf = Cdf.MakeCdfFromList(cohort_dict.values())
 
 xs = sorted(pmf.Values())
 ys = map(lambda(x):0.05, xs)
+ys2 = map(lambda(x):0.01, xs)
 myplot.Pmf(pmf)
 myplot.Plot(xs, ys)
+myplot.Plot(xs, ys2)
 myplot.Show(title="Distribution of diagnoses")
 
 #graph indicates any cohort with 4 or more cases is statistically significant
@@ -36,25 +49,13 @@ myplot.Show(title="Distribution of diagnoses")
 print 'odds of at least one cohort in 100 generating a 5% event', 1 - (0.95 ** 100)
 
 #by simulation
-experimental_success = 0.0
-cohort_count = defaultdict(int)
+print  'odds of 5% event based on 1000 experiments = ', monte_carlo_cohorts(100, 100, 10, 4, 1000)
 
-for experiment in range(10000): 
-    experiment_succeeded = False
-    for year in range(10):
-        for cohort in range(100):
-            experiment_succeeded = False
-            cohort_count[cohort] += year_of_cohort(100)
-            if cohort_count[cohort] >= 4:
-                experiment_succeeded = True
-                break
-        if experiment_succeeded:
-            experimental_success += 1.0
-            break
-      
-print  'odds of 5% event based on 10000 experiments = ', (experimental_success / 10000.0)
-
+#by analysis
 print 'odds of at least one cohort in 100 generating a 1% event', 1 - (0.99 ** 100)
+
+#by simulation
+print 'odds of a 1% event based on 1000 experiments =' , monte_carlo_cohorts(100, 100, 10, 5, 1000)
 
 print 'There are 91x91 10x10 blocks in a 100x100 grid. the chance of at least one having a 5% event is 1 - (0.95^(91*91))=', (1 - (0.95 ** (91 ** 2)))
 
